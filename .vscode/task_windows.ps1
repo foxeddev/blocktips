5c7e5069be079e4e8337dfced0a0b934d31c76ae
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2025-08-11 13:44:02 +0200

    Fix windows task

--- .vscode/task_windows.ps1
+++ .vscode/task_windows.ps1
@@ -1,10 +1,6 @@
 if (-not (Test-Path .venv)) {
     python -m venv .venv
-    .venv\Scripts\Activate.ps1
-    pip install -r requirements.txt
-    } else {
-    .venv\Scripts\Activate.ps1
+    .\.venv\Scripts\python -m pip install -r requirements.txt
 }
-
 clear
-beet watch
+.\.venv\Scripts\python -m beet watch
