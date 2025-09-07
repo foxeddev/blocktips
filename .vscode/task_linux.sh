bf18956f94d4554c13f608ad06e6c4473b7f0d7c
FoxedDev <169158948+foxeddev@users.noreply.github.com>
2025-09-07 16:44:18 +0200

    Remove unnecessary sleep commands from task_linux.sh

--- .vscode/task_linux.sh
+++ .vscode/task_linux.sh
@@ -4,10 +4,8 @@ if [ ! -d .venv ]; then
     python3 -m venv .venv
     source .venv/bin/activate
     pip install -r requirements.txt
-    sleep 0.1
 else
     source .venv/bin/activate
-    sleep 0.1
 fi
 
 clear
